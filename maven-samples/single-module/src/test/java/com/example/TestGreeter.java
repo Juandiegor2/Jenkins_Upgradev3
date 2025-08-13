4bc4a9247d8da35aea02999a55f34b31c4fecf52
Juandiegor2 <juandiegor2@hotmail.com>
2025-08-13 12:16:39 -0500

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,12 +27,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
  
-   @Test
-  public void Intentionally_Failed() {
-   assertTrue(false);
-  }
-  
-  @Test
+ @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
 
